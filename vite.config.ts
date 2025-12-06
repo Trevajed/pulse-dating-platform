c4f3ed9799b21ec67fafb4c1866e5b05aaafb626
Trevajed <67669961+Trevajed@users.noreply.github.com>
2025-12-06 07:06:26 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
-import build from '@hono/vite-build/cloudflare-pages'
+import pages from '@hono/vite-build/cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
-    build(),
+    pages({
+      entry: 'src/index.tsx'
+    }),
     devServer({
       adapter,
       entry: 'src/index.tsx'
     })
-  ]
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    minify: true,
+    rollupOptions: {
+      output: {
+        format: 'es'
+      }
+    }
+  }
 })
